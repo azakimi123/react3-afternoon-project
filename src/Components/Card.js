f8964abae59598215d7805c24c0962351ddd8111
azakimi123 <aaron.zakimi@gmail.com>
2020-06-29 16:38:11 -0600

    adjusted styling

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,9 +8,10 @@ function Card(props) {
         <div className='card'>
             <section className='inner-text'>
             <h2 className='person-name'>{props.info.firstName} {props.info.lastName}</h2>
-            <h4>From: </h4> <p>{props.info.city} {props.info.country}</p>
-            <h4>Employer: </h4><p>{props.info.employer}</p>
-            <section>
+            <p><span className='bold'>From: </span>{props.info.city} {props.info.country}</p>
+            <p><span className='bold'>Job Title: </span>{props.info.title}</p>
+            <p><span className='bold'>Employer: </span>{props.info.employer}</p>
+            <section className='movies'>
                 <h4>Favorite Movies: </h4>
                 <ol>1. {props.info.favoriteMovies[0]}</ol>
                 <ol>2. {props.info.favoriteMovies[1]}</ol>
