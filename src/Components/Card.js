f7eabfc049445e35f092ac7f878801af5b5d6d9a
azakimi123 <aaron.zakimi@gmail.com>
2020-06-29 16:07:35 -0600

    name underline added

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,14 +7,14 @@ function Card(props) {
     return(
         <div className='card'>
             <section className='inner-text'>
-            <h2>{props.info.firstName} {props.info.lastName}</h2>
+            <h2 className='person-name'>{props.info.firstName} {props.info.lastName}</h2>
             <h4>From: </h4> <p>{props.info.city} {props.info.country}</p>
             <h4>Employer: </h4><p>{props.info.employer}</p>
             <section>
                 <h4>Favorite Movies: </h4>
-                <ol>1.{props.info.favoriteMovies[0]}</ol>
-                <ol>2.{props.info.favoriteMovies[1]}</ol>
-                <ol>3.{props.info.favoriteMovies[2]}</ol>
+                <ol>1. {props.info.favoriteMovies[0]}</ol>
+                <ol>2. {props.info.favoriteMovies[1]}</ol>
+                <ol>3. {props.info.favoriteMovies[2]}</ol>
             </section>
             </section>
         </div>
